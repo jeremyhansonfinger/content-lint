d4989a66d0e599053302dcc9fe19f06c8bff2a31
Jeremy Hanson-Finger <jeremy.hansonfinger@shopify.com>
2016-10-26 10:31:25 -0400

    Change command to bin/

--- spec/bin/bin_content_style_spec.rb
+++ spec/bin/bin_content_style_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Run command `content-style text/html`', type: :aruba do
   context 'when an html file containing a violation is located in `test/html` and config file is
           located at `config/content-style.yml`' do
     before(:each) do
-      run_simple 'bundle exec content-style test/html'
+      run_simple 'bin/content-style test/html'
     end
     it 'writes an error message for violation to stdout' do
       expect(last_command_started.stdout.chomp).to include 'dropdown'

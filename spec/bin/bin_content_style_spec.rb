31a930da51be72a3c51468c254bb960067ea9178
Jeremy Hanson-Finger <jeremy.hansonfinger@shopify.com>
2016-10-31 17:00:10 -0400

    Exclude files (#5)
    * Add ability to exclude files
    
    * Update readme with excluded-files

--- spec/bin/bin_content_style_spec.rb
+++ spec/bin/bin_content_style_spec.rb
@@ -13,3 +13,15 @@ RSpec.describe 'Run command `content-style text/html`', type: :aruba do
     end
   end
 end
+
+RSpec.describe 'Run command `content-style text/html`', type: :aruba do
+  context 'when a file marked for exclusion contains a violation' do
+    before(:each) do
+      run_simple 'bin/content-style test/html'
+    end
+    it 'does not write error message for violation in excluded file' do
+      expect(last_command_started.stdout.chomp).to include 'dropdown'
+      expect(last_command_started.stdout.chomp).not_to include 'droop down'
+    end
+  end
+end
